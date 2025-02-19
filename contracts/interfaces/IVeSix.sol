2d7296b6b60f18c03e0f41afcc0c3dbb449dadb8
0xPhant0m <108011905+fGhostPhant0m@users.noreply.github.com>
2025-02-19 09:52:30 -0500

    Update IVeSix.sol

--- contracts/interfaces/IVeSix.sol
+++ contracts/interfaces/IVeSix.sol
@@ -12,6 +12,10 @@ interface IVeSix {
     function balanceOfNFTAt(uint256 tokenId, uint256 timestamp) external view returns (uint256);
     function ownerOf(uint256 tokenId) external view returns (address);
     function isApprovedOrOwner(address spender, uint256 tokenId) external view returns (bool);
+    function lockedToken() external view returns (address);
+    function increaseLockAmount(uint256 tokenId, uint128 additionalAmount, uint256 deadline,
+     uint128 minMultiplier) external;
+    function getCurrentMultiplier(uint256 tokenId) external view returns (uint128);
     function deposit_for(uint256 tokenId, uint256 value) external;
     function point_history(uint256 epoch) external view returns (Point memory);
     function user_point_history(uint256 tokenId, uint256 loc) external view returns (Point memory);
